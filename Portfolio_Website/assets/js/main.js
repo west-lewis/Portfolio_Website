9af12bed1ba35cd847773e36b29c91f84d1403ab
west-lewis <103295423+west-lewis@users.noreply.github.com>
2022-11-15 22:17:22 -0800

    so much good work

--- Portfolio_Website/assets/js/main.js
+++ Portfolio_Website/assets/js/main.js
@@ -159,12 +159,6 @@
     });
   }
 
-  /**
-   * Initiate glightbox 
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
 
   /**
    * Testimonials slider
@@ -296,4 +290,24 @@
     }
   }
 
+    var carousel = document.querySelector('.carousel');
+  var cellCount = 3;
+  var selectedIndex = 0;
+
+  function rotateCarousel() {
+    var angle = selectedIndex / cellCount * -360;
+    carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+  }
+
+  var prevButton = document.querySelector('.previous-button');
+  prevButton.addEventListener( 'click', function() {
+    selectedIndex--;
+    rotateCarousel();
+  });
+
+  var nextButton = document.querySelector('.next-button');
+  nextButton.addEventListener( 'click', function() {
+    selectedIndex++;
+    rotateCarousel();
+  });
 })()
