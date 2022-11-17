b962c142a3df403db60aab3abd13c34313ff4ac7
west-lewis <103295423+west-lewis@users.noreply.github.com>
2022-11-17 13:38:08 -0800

    dga

--- Portfolio_Website/assets/js/main.js
+++ Portfolio_Website/assets/js/main.js
@@ -290,6 +290,16 @@
     }
   }
 
+  var carousel = document.querySelector('.carousel');
+  var cells = carousel.querySelectorAll('.carousel__cell');
+  var selectedIndex = 0;
+  var cellWidth = carousel.offsetWidth;
+  var cellHeight = carousel.offsetHeight;
+  var isHorizontal = true;
+  var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+  var radius, theta;
+  // console.log( cellWidth, cellHeight );
+
     var carousel = document.querySelector('.carousel');
   var cellCount = 3;
   var selectedIndex = 0;
@@ -310,4 +320,45 @@
     selectedIndex++;
     rotateCarousel();
   });
+
+  function changeCarousel() {
+    cellCount = cellsRange.value;
+    theta = 360 / cellCount;
+    var cellSize = isHorizontal ? cellWidth : cellHeight;
+    radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+    for ( var i=0; i < cells.length; i++ ) {
+      var cell = cells[i];
+      if ( i < cellCount ) {
+        // visible cell
+        cell.style.opacity = 1;
+        var cellAngle = theta * i;
+        cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+      } else {
+        // hidden cell
+        cell.style.opacity = 0;
+        cell.style.transform = 'none';
+      }
+    }
+  
+    rotateCarousel();
+  }
+  
+  var orientationRadios = document.querySelectorAll('input[name="orientation"]');
+  ( function() {
+    for ( var i=0; i < orientationRadios.length; i++ ) {
+      var radio = orientationRadios[i];
+      radio.addEventListener( 'change', onOrientationChange );
+    }
+  })();
+  
+  function onOrientationChange() {
+    var checkedRadio = document.querySelector('input[name="orientation"]:checked');
+    isHorizontal = checkedRadio.value == 'horizontal';
+    rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+    changeCarousel();
+  }
+  
+  // set initials
+  onOrientationChange();
+  
 })()
